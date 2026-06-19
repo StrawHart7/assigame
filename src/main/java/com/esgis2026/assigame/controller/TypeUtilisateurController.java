9822595b74bc6605190cd5c0b7dba344a3b27cb6
StrawHart <test@gmail.com>
2026-06-19 18:27:09 +0000

    Ready to merge

--- src/main/java/com/esgis2026/assigame/controller/TypeUtilisateurController.java
+++ src/main/java/com/esgis2026/assigame/controller/TypeUtilisateurController.java
@@ -3,14 +3,7 @@ package com.esgis2026.assigame.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.esgis2026.assigame.entity.TypeUtilisateur;
 import com.esgis2026.assigame.service.TypeUtilisateurService;
--- src/main/java/com/esgis2026/assigame/entity/Utilisateur.java
+++ src/main/java/com/esgis2026/assigame/entity/Utilisateur.java
@@ -3,15 +3,7 @@ package com.esgis2026.assigame.entity;
 
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
